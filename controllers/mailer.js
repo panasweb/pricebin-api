ca5cf51ec1c623fe580f36e4e91d902b082f102d
eric chao <ericjardon@hotmail.com>
2022-05-28 19:47:13 -0500

    email integration with sendgrid. Add new key to .env

---
+++ controllers/mailer.js
@@ -0,0 +1,27 @@
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail')
+
+
+module.exports.testMail = function(req, res) {
+    sgMail.setApiKey(process.env.SG_API_KEY);
+    const msg = {
+      to: 'ericjardon@hotmail.com', // Change to your recipient
+      from: 'geebproject@gmail.com', // Change to your verified sender
+      subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Email sent')
+        return res.status(200).send("Mail sent");
+      })
+      .catch((error) => {
+        console.error(error)
+        return res.status(500).send("Error", error);
+      })
+    
+}
+
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const converter = require('./api/converter');
+const mailer = require('../controllers/mailer');
 
 /* GET home page. */
 router.post('/convert', converter.getCurrencyRate);  // params in req.body
+router.post('/mail/test', mailer.testMail);
 
 router.get('/', function(req, res, next) {
-  
   res.redirect("/doc");
-
 });
 
 
