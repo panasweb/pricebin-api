36d7e7d7b7f58f353e2720f5619b9e939b841216
eric chao <ericjardon@hotmail.com>
2022-05-28 20:05:16 -0500

    work on email template for tokens

--- controllers/mailer.js
+++ controllers/mailer.js
@@ -1,6 +1,22 @@
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
-const sgMail = require('@sendgrid/mail')
+const sgMail = require('@sendgrid/mail');
+const User = require('../models/User');
+const VERIFIED_SENDER = 'geebproject@gmail.com';
+
+const MAIL_BODY = (username, verifyLink) => (`
+Estimado ${username},
+
+¡Te saludamos por parte del equipo Pricebin!
+Por favor has click en el siguiente link para verificar tu cuenta:
+
+${verifyLink}
+
+¡Bienvenido al barrio!
+Atte.
+Pricebin 🍜
+`);
+
 sgMail.setApiKey(process.env.SG_API_KEY);
 
 
@@ -25,3 +41,33 @@ module.exports.testMail = function(req, res) {
     
 }
 
+exports.send2FAMail = async function(req, res) {
+  let {UserKey, email} = req.body;
+
+  if (!email) {
+    if (!UserKey) {
+      return res.status(401).send("Missing UserKey and email parameters")
+    }
+    const user = await User.findById(UserKey);
+    email = user.email;
+  }
+
+  const msg = {
+    to: email, // recipient
+    from: VERIFIED_SENDER, // verified sender
+    subject: 'Verifica tu cuenta en Pricebin',
+    text: MAIL_TEXT,
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  }
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent')
+      return res.status(200).send("Mail sent");
+    })
+    .catch((error) => {
+      console.error(error)
+      return res.status(500).send("Error", error);
+    })
+
+}
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,10 @@ const mailer = require('../controllers/mailer');
 
 /* GET home page. */
 router.post('/convert', converter.getCurrencyRate);  // params in req.body
+
+// Params
 router.post('/mail/test', mailer.testMail);
+router.post('/mail/send', mailer.send2FAMail);
 
 router.get('/', function(req, res, next) {
   res.redirect("/doc");
