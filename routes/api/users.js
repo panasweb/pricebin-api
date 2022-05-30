c1971848fcc9b9546c45030fb7b024b85a221040
Georgina <gina10111@hotmail.com>
2022-05-30 00:11:13 -0500

    delete unused route to avoid error

--- routes/api/users.js
+++ routes/api/users.js
@@ -25,8 +25,6 @@ router.post("/product/clear", user.clearCurrentList);
 
 router.post("/product/update", user.updateCurrentList);
 
-router.post("/product/save", user.saveCurrentList, list.create);
-
 router.post("/stats/recalculate", user.recalculateUserStats);
 
 router.post("/stats/cool", user.getCoolStats);  // fav product, fav store
